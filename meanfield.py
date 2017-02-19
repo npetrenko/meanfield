445b85f6e39dbd0d6685dcde1a7dc0febb42d95b
Nikita Petrenko <npetrenko97@gmail.com>
2017-02-19 20:59:24 +0300

    fixed major loss bag and added some comments

--- meanfield.py
+++ meanfield.py
@@ -6,7 +6,15 @@ import gc
 sample_size = 1
 
 class Dense():
-    def __init__(self, dim, input_layer, act=tf.nn.relu, params=None, samp_size=sample_size, prior=3):
+    def __init__(self, dim, input_layer, act=tf.nn.relu, samp_size=sample_size, prior=3):
+        '''
+        :param dim: number of neurons in the layer
+        :param input_layer: input layer calss object
+        :param act: tensorflow activation function
+        :param samp_size: size of sample on monte carlo step
+        :param prior: standard deviation of prior
+        '''
+
         self.dim = dim
         self.inp_dim = input_layer.dim
 
@@ -19,40 +27,64 @@ class Dense():
         self.sigma_const = tf.Variable(np.random.normal(size=dim, scale=0.01, loc=0), dtype=tf.float32)
         self.sigma_const = tf.log(tf.exp(self.sigma_const + 0.1) + 1)
 
+        #sample of activation matrixes and biases
         activation_matrix = tf.random_normal(shape=[samp_size] + shape, dtype=tf.float32,
                                              stddev=1) * self.sigma + self.mean
         bias = tf.random_normal(shape=[samp_size, 1, dim], dtype=tf.float32, stddev=1) * self.sigma_const + self.mean_const
 
+        #calculate matrix multiplication for each sample
         l = []
         for i in range(samp_size):
             temp = tf.matmul(input_layer.output[i], activation_matrix[i])
             l.append(temp)
         temp = tf.pack(l, axis=0)
-        #self.output = tf.transpose(act(tf.transpose(temp, [1, 0, 2]) + bias), [1, 0, 2])
+
         self.output = act(temp + bias)
         print(self.output)
 
+        #...
         l1 = tf.reduce_sum(-tf.log(np.sqrt(2 * pi) * self.sigma)) + tf.reduce_sum(
             -tf.log(np.sqrt(2 * pi) * self.sigma_const))
+        #prior loss
         l2 = (tf.reduce_sum(-activation_matrix ** 2) + tf.reduce_sum(- bias ** 2)) / (2 * prior)
+
+        #feeding loss to the next layer
         self.loss = l1 - l2
         self.loss += input_layer.loss
 
 class Input():
     def __init__(self, dim, sample_size=sample_size):
+        '''
+
+        :param dim: number of sensors in the first layer
+        :param sample_size: size of sample on monte carlo step
+        '''
         self.input = tf.placeholder(shape=tuple([sample_size, None] + [dim]), dtype=tf.float32)
         self.output = self.input
         self.dim = dim
         self.loss = 0
 
 class Model():
+
     def terminate(self):
+        '''
+        close tensorflow session
+        '''
         self.sess.close()
+
     def __init__(self, input, output, optimizer=tf.train.AdamOptimizer(0.001)):
+        '''
+        :param input: input node of the neural network
+        :param output: output node of the network
+        :param optimizer: tensorflow optimizer
+        '''
         self.sess = tf.Session()
         self.loss = output.loss
+
+        #create placeholder for ys
         self.y_ph = tf.placeholder(shape=(sample_size, None, 1), dtype=tf.float32)
 
+        #parameter which helps to build the final loss
         self.loss_final = False
 
         self.input = input
@@ -60,31 +92,48 @@ class Model():
 
         self.optimizer = optimizer
 
-        self.sess.run(tf.initialize_all_variables())
-
     def fit(self, X, y, nepoch, batchsize, log_freq=100, valid_set = None, shuffle_freq = 1):
-        in_tens = np.repeat([X], sample_size, axis=0)#.reshape((sample_size, -1, 1))
-        in_tens_y = np.repeat([y], sample_size, axis=0)#.reshape((sample_size, -1, 1))
+
+        #create input suitable for feeding into the input node
+        in_tens = np.repeat([X], sample_size, axis=0)
+        in_tens_y = np.repeat([y], sample_size, axis=0)
         nbatch = int(len(X)/batchsize)
 
         if not self.loss_final:
+            #create object for storing part of loss dependent on variables:
+            self.var_loss = self.loss
+
             loss = tf.reduce_sum(((self.output.output - self.y_ph) ** 2)) / (2 * 5 ** 2) + self.loss / (nbatch * 1.)
             loss = loss / sample_size
-            self.loss += loss
+            self.loss = loss
             self.optimizer = self.optimizer.minimize(self.loss)
             self.sess.run(tf.initialize_all_variables())
             self.loss_final = True
 
+            #remember batchsize in case of change
+            self.batchsize = batchsize
+
+        #reconfigure loss in case of batch size change
+        if self.loss_final and self.batchsize != batchsize:
+            loss = tf.reduce_sum(((self.output.output - self.y_ph) ** 2)) / (2 * 5 ** 2) + self.var_loss / (nbatch * 1.)
+            loss = loss / sample_size
+            self.loss = loss
+
+
         for epoch in range(nepoch):
+
             if epoch % log_freq == 0:
                 preds = self.predict(X, samplesize=20)
                 train_mse = np.sqrt(np.mean((preds-y) ** 2))
+
                 if valid_set is not None:
                     preds = self.predict(valid_set[0], samplesize=20)
                     valid_mse = np.sqrt(np.mean((preds - valid_set[1]) ** 2))
                     print('epoch: {} \n train error: {} \n valid_error: {} \n\n\n'.format(epoch, train_mse, valid_mse))
+
                 else:
                     print('epoch: {} \n train error: {} \n\n\n'.format(epoch, train_mse))
+
             for i in range(nbatch):
                 self.sess.run(self.optimizer,
                               feed_dict={
@@ -94,14 +143,20 @@ class Model():
 
             if epoch % shuffle_freq == 0:
                 shuffle = np.random.permutation(in_tens.shape[1])
+                #not running gc right after shuffle causes memory leak
                 gc.collect()
                 in_tens = in_tens[:, shuffle, :]
                 in_tens_y = in_tens_y[:, shuffle, :]
 
 
     def predict(self, X, samplesize=250, return_distrib=False):
+        '''
+        :param samplesize: size of prediction sample of variables
+        :param return_distrib: whether to return a whole set of samples of only the mean value
+        '''
         n = int(samplesize / sample_size) + 1
         X = np.repeat([X], sample_size, axis=0)
+        print(X.shape)
         preds = None
         if return_distrib:
             for i in range(n):
