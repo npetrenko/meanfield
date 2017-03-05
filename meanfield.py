d29b7de8ced73ccf53b20ebb9464162877e6ae74
Nikita Petrenko <npetrenko97@gmail.com>
2017-03-05 12:19:00 +0300

    everything works

--- meanfield.py
+++ meanfield.py
@@ -71,7 +71,8 @@ class Dense(Layer):
 
         #matrixes = tt.batched_dot(input_layer.output, activation_matrix)
 
-        mb, _ = th.scan(lambda i: tt.dot(input_layer.output[i], activation_matrix[i]), sequences=[tt.arange(sample_size)], non_sequences=[input_layer.output, activation_matrix])
+        mb, _ = th.scan(lambda i, m1, m2: tt.dot(m1[i], m2[i]), sequences=[tt.arange(sample_size)],
+                        non_sequences=[input_layer.output, activation_matrix])
 
         # index = np.array([[i,i] for i in range(sample_size)], dtype='int')
 
